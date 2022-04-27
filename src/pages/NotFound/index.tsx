10079912ae8098d2d95c825e414d129729ff83a0
Douglas <douglas04041996@hotmail.com>
2022-04-27 14:22:53 -0300

    comit

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,7 +2,9 @@ import styles from './NotFound.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
     <div
       className={classNames({
@@ -13,7 +15,7 @@ export default function NotFound() {
       <div className={styles.voltar}>
         <button
           onClick={() => {
-            history.back();
+            navigate(-1);
           }}
         >
           {'< Voltar'}
